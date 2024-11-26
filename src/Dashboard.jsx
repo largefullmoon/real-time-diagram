3bebf49e2fc60dccb29176e80c88239a236e0e0c
largefullmoon <largefullmoon@gmail.com>
2024-11-27 01:40:43 +0200

    update with new feedback

--- src/Dashboard.jsx
+++ src/Dashboard.jsx
@@ -11,6 +11,9 @@ import {
     Handle,
     useReactFlow,
     Position,
+    BaseEdge,
+    EdgeLabelRenderer,
+    getBezierPath,
 } from '@xyflow/react';
 import '@xyflow/react/dist/style.css';
 import Edit from './assets/edit.svg'
@@ -19,11 +22,35 @@ import LogOut from './assets/logout.svg'
 import { Tooltip } from 'react-tooltip'
 import 'react-tooltip/dist/react-tooltip.css'
 import { useNavigate } from 'react-router-dom';
+import { data } from 'autoprefixer';
 const initialNodes = [
 ];
 const initialEdges = [];
 export const NormalNode = ({ data, onDragStart }) => {
     const [isToolbarOpen, setIsToolbarOpen] = useState(false);
+    const [html, setHtml] = useState("")
+    useEffect(() => {
+        if (data.queue) {
+            let consumersString = ""
+            data.queue.consumers.map((consumer, index) => {
+                if (index == 0) {
+                    consumersString += `<p>
+                        consumers:
+                    </p>`
+                }
+                consumersString += `<p class="pl-5">
+                        channelName : ${consumer?.channelName}
+                    </p>
+                    <p class="pl-5">
+                        consumerTag : ${consumer?.consumerTag}
+                    </p>
+                    <p class="pl-5">
+                        isActive : ${consumer?.isActive}
+                    </p>`
+            })
+            setHtml(consumersString)
+        }
+    }, [html])
     return (
         <a data-tooltip-id="my-tooltip" data-tooltip-html={
             data.exchange ?
@@ -46,14 +73,12 @@ export const NormalNode = ({ data, onDragStart }) => {
                     name : ${data.queue.queueName}
                 </p>
                 <p>
-                    messageCount : ${data.queue.messageCount}
-                </p>
-                <p>
                     isDurable : ${data.queue.isDurable ? "true" : "false"}
                 </p>
                 <p>
                     guid : ${data.queue.guid}
                 </p>
+                ${html}
             </div>
             `
         }
@@ -104,6 +129,53 @@ export const NormalNode = ({ data, onDragStart }) => {
 };
 const nodeTypes = {
     normal: NormalNode,
+};
+const CustomEdge = ({
+    id,
+    sourceX,
+    sourceY,
+    targetX,
+    targetY,
+    sourcePosition,
+    targetPosition,
+    data = {},
+    markerEnd,
+}) => {
+    const { setEdges } = useReactFlow();
+    const [edgePath, labelX, labelY] = getBezierPath({
+        sourceX,
+        sourceY,
+        sourcePosition,
+        targetX,
+        targetY,
+        targetPosition,
+    });
+
+    const onEdgeClick = () => {
+        setEdges((edges) => edges.filter((edge) => edge.id !== id));
+    };
+
+    return (
+        <>
+            <BaseEdge path={edgePath} markerEnd={markerEnd} />
+            <EdgeLabelRenderer>
+                <div
+                    style={{
+                        position: 'absolute',
+                        transform: `translate(-50%, -50%) translate(${labelX}px,${labelY}px)`,
+                        fontSize: 16,
+                        pointerEvents: 'all',
+                    }}
+                    className="flex items-center justify-center nodrag nopan"
+                >
+                    {data.count}
+                </div>
+            </EdgeLabelRenderer>
+        </>
+    );
+}
+const edgeTypes = {
+    buttonedge: CustomEdge,
 };
 const Dashboard = () => {
     const navigate = useNavigate();
@@ -276,7 +348,7 @@ const Dashboard = () => {
                         data: { label: queue.queueName, color: "purple", queue: queue },
                         type: 'normal'
                     });
-                    links.push({ id: `link_queue_${queue.guid}`, source: `exchange_${attachExchange.exchange.guid}`, target: `queue_${queue.guid}` })
+                    links.push({ id: `link_queue_${queue.guid}`, source: `exchange_${attachExchange.exchange.guid}`, target: `queue_${queue.guid}`, type: "buttonedge", data: { count: queue.messageCount } })
                 });
             }
         });
@@ -296,7 +368,7 @@ const Dashboard = () => {
     const [nodes, setNodes, onNodesChange] = useNodesState([]);
     const [edges, setEdges, onEdgesChange] = useEdgesState([]);
     const onConnect = useCallback(
-        (params) => setEdges((eds) => addEdge(params, eds)),
+        (params) => setEdges((eds) => addEdge({ ...params, type: "buttonedge" }, eds)),
         [setEdges],
     );
     return (
@@ -315,13 +387,13 @@ const Dashboard = () => {
                     onNodesChange={onNodesChange}
                     onEdgesChange={onEdgesChange}
                     onConnect={onConnect}
-                    // colorMode={'dark'}
                     fitView
                     fitViewOptions={{
-                        
+
                         padding: 0.2,
                     }}
                     nodeTypes={nodeTypes}
+                    edgeTypes={edgeTypes}
                 >
                     <Controls />
                     <MiniMap />
